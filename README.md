679fda964d7a26a67857cc5ba52d92aab259f50c
Neos21 <neos21@gmail.com>
2022-10-02 22:53:14 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Practice VB.NET
+
+Practice VB.NET
+
+```winbatch
+@REM Windows Command Prompt : Compile VB.NET Script
+CMD> C:\Windows\Microsoft.NET\Framework\v4.0.30319\vbc.exe .\template.vb
+
+@REM Execute
+CMD> template.exe
+```
+
+## Links
+
+- [Neo's World](https://neos21.net/)
---
+++ template.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Module1
+  Sub Main()
+    ' Template
+    Console.WriteLine("Hello World")
+    ' Sleep
+    System.Threading.Thread.Sleep(1000)
+    ' Print Echo
+    Console.WriteLine("Please Enter key to continue...")
+    ' Pause
+    Console.ReadKey()
+    ' Exit
+    Environment.Exit(0)
+  End Sub
+End Module
